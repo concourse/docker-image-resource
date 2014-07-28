dd0ccc3d48b74e6ab6d6eae3c6913fa6cd2d849f
Alex Suraci <suraci.alex@gmail.com>
2014-07-27 17:02:58 -0700

    fix borked docker starting

--- assets/common.bash
+++ assets/common.bash
@@ -19,29 +19,9 @@ function start_docker() {
 
   /etc/init.d/docker.io start
 
-  docker_pid=$(cat /var/run/docker.io.pid)
-
   until docker info >/dev/null 2>&1; do
     echo waiting for docker to come up...
     sleep 0.5
-
-    if ! kill -0 $docker_pid >/dev/null 2>&1; then
-      echo "docker exited."
-
-      if [ -f /var/log/docker.out.log ]; then
-        echo
-        echo "stdout:"
-        cat /var/log/docker.out.log
-      fi
-
-      if [ -f /var/log/docker.err.log ]; then
-        echo
-        echo "stderr:"
-        cat /var/log/docker.err.log
-      fi
-
-      exit 1
-    fi
   done
 }
 
